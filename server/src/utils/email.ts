abf3c60c4947e288a6d27eca23c47d754fa6f1f7
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2025-11-01 19:52:42 +0530

    nodemailer config

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -3,8 +3,9 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to: string, subject: string, text: string) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
+        service: "Gmail",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER, // your email
             pass: process.env.EMAIL_PASSWORD, // app password or generated key
