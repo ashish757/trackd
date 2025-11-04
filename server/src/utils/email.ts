4ba1c01576e42c615ad337598334f8bd71282add
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2025-11-04 09:26:34 +0530

    resend mail service setup

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -3,11 +3,19 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (to: string, subject: string, body: string) => {
-    await resend.emails.send({
-        from: 'onboarding@resend.dev',
+
+    const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
         to,
         subject,
-        html: body
+        html: body,
     });
+
+    if (error) {
+        return console.log("ERRROR FROM RESEND : ", { error });
+    }
+
+    console.log({ data });
+    return data;
 }
 
