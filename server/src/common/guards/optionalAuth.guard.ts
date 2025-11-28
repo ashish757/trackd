ef5efee2872ff0c929a0cb384857be8a468a3696
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2025-11-28 22:22:07 +0530

    created optional auth guard

---
+++ server/src/common/guards/optionalAuth.guard.ts
@@ -0,0 +1,39 @@
+import {
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '../../modules/auth/jwt.service';
+
+@Injectable()
+export class OptionalAuthGuard implements CanActivate {
+    constructor(private readonly jwtService: JwtService) {}
+
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const authHeader = request.headers['authorization'];
+
+        if (!authHeader) {
+            throw new UnauthorizedException('No Authorization header provided');
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        // Simple token check (replace with JWT verification)
+        const { payload, error } = this.jwtService.verify(token, 'access');
+
+        if (error) {
+            request.isAuthenticated = false;
+            request.user = {};
+            return true;
+        }
+
+        request.isAuthenticated = true;
+        request.user = payload;
+        return true;
+    }
+}
