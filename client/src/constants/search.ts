a1b4f83315978e9c7f1ede8dc39683161c3f8537
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2025-12-15 11:36:32 +0530

    created a file to store the constants for TMDB and searching functionality

---
+++ client/src/constants/search.ts
@@ -0,0 +1,13 @@
+/**
+ * Search-related constants
+ */
+
+export const SEARCH_CONFIG = {
+  /** Debounce delay for search input in milliseconds */
+  DEBOUNCE_DELAY: 300,
+  /** Minimum character length to trigger search */
+  MIN_SEARCH_LENGTH: 2,
+  /** Maximum number of suggestions to show in dropdown */
+  MAX_SUGGESTIONS: 10,
+} as const;
+
