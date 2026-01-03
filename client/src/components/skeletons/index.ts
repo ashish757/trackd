8dc6b9e0a65bdbc706d48fc5860bda4238bf5bf3
Ashish Raj Singh <ashishrajsingh75@gmail.com>
2026-01-03 16:09:35 +0530

    unified the skeletons for movie cards

--- client/src/components/skeletons/index.ts
+++ client/src/components/skeletons/index.ts
@@ -3,11 +3,10 @@
  * Centralized exports for all loading skeleton components
  */
 
-export { default as MovieCardSkeleton } from './MovieCardSkeleton';
 export { default as StatCardSkeleton } from './StatCardSkeleton';
 export { default as ProfileHeaderSkeleton } from './ProfileHeaderSkeleton';
 export { default as SearchSuggestionSkeleton } from './SearchSuggestionSkeleton';
 export { default as FriendListItemSkeleton } from './FriendListItemSkeleton';
-export { default as MovieGridSkeleton } from './MovieGridSkeleton';
+export { default as MovieCardSkeleton } from './MovieCardSkeleton';
 export { default as UserCardSkeleton } from './UserCardSkeleton';
 
